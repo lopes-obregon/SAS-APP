f607497feabc34f7889b87470aa8ec702bcb0c7a
Samara0liveira <164580150+Samara0liveira@users.noreply.github.com>
2024-04-01 12:20:59 -0400

    new

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as Animatable from 'react-native-animatable'
+
+import {useNavigation} from '@react-navigation/native'
 
 export default function Welcome(){
+    const navigation = useNavigation();
     return (
         <View style={styles.container}>
 
             <View style={styles.containerLogo}>
-                <Image
+                <Animatable.Image
+                animation="flipInY"
                     source={require('../../assets/welcome.png')}
                     style= {{width:'100%'}}
                     resizeMode="contain"
                 />
             </View>
 
-            <View style={styles.containerForm}>
+            <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
+
+            <Image
+                    source={require('../../assets/teste.png')}
+                    style= {{width:'100%'}}
+                    resizeMode="contain"
+                />
+
                 <Text style={styles.title}>O seu atendimento médico no conforto da sua casa</Text>
                 
 
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Login</Text>
+                <TouchableOpacity 
+                style={styles.button}
+                onPress={ () => navigation.navigate('SignIn')}
+                >
+                    <Text style={styles.buttonText}>LOGIN</Text>
                 </TouchableOpacity>
 
-            </View>
+            </Animatable.View>
 
 
         </View>
@@ -32,10 +47,50 @@ export default function Welcome(){
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        
+        backgroundColor:"#408755",
     },
     containerLogo:{
-        
-        backgroundColor:"#408755"
+        flex:0,
+        backgroundColor:"#408755",
+        justifyContent: 'center',
+        alignItems: 'center'
     },
+    containerForm:{
+        flex:1,
+        backgroundColor:"#FFF",
+        borderTopLeftRadius: 25,
+        borderTopRightRadius:25,
+        paddingStart: '5%',
+        paddingEnd: '5%',
+
+    },
+
+    title:{
+        fontSize:24,
+        color:'#408755',
+        fontWeight: 'bold',
+        marginTop:28,
+        marginBottom:12,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button:{
+        position: 'absolute',
+        backgroundColor:'#408755',
+        borderRadius:50,
+        paddingVertical:8,
+        width: '80%',
+        alignSelf: 'center',
+        bottom:'15%',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+
+    },
+    buttonText:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFF',
+    }
+
 })
